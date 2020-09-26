c98ffe65d1b78ec6458b7d10dca4855c9b99e295
marala <sailokandham.marala@hpe.com>
2020-09-26 17:19:04 +0530

    Browser satck code

---
+++ Mavenproject/SeleniumCloud/src/test/java/com/cloudtesting/SauceTest.java
@@ -0,0 +1,27 @@
+package com.cloudtesting;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SauceTest extends BrowserStackBaseTest {
+
+    public void doLogin() {
+        driver.get("https://www.saucedemo.com/index.html");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test(priority = 1)
+    public void checkInventoryItemTest() {
+        doLogin();
+        Assert.assertTrue(driver.findElements(By.cssSelector(".inventory_item")).size() == 6);
+    }
+
+    @Test(priority = 2)
+    public void checkAddToCartButtonTest() {
+        doLogin();
+        Assert.assertTrue(driver.findElements(By.xpath("//button[text()='ADD TO CART']")).size() == 6);
+    }
+}
