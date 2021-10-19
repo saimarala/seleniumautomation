96f8e46a17a1eefce6825b5f92918797fe8b9f8c
saimarala <71916594+saimarala@users.noreply.github.com>
2021-10-19 20:44:54 +0530

    Add files via upload

---
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Methodoverlading/MethodOverLoading.java
@@ -0,0 +1,42 @@
+package Methodoverlading;
+
+public class MethodOverLoading {
+
+    public static void main(String[] args) {
+        MethodOverLoading obj=new MethodOverLoading();
+        obj.sum();
+        obj.sum(10);
+        obj.sum(20.22);
+        obj.sum(10,20);
+
+    }
+    //you can not create a method inside a method
+    //duplicate methods i.e same method name with same number of arguments are not allowed
+    //method overloading --> when method name is same with different arguments
+    //or input parameters within the same class
+
+
+     void sum() {
+        System.out.println("sum method--zero param");
+    }
+    void sum(int i){
+        System.out.println("sum method--1 param");
+        System.out.println(i);
+    }
+    void sum(double i){
+        System.out.println("sum method--1 param");
+        System.out.println(i);
+    }
+    void sum(int i,int j){
+        System.out.println("sum method--2 param");
+        System.out.println(i+j);
+    }
+
+    public static void main(int a) {//we can overload main method also
+        System.out.println("main");
+    }
+
+    public static void main(int a,int b) {
+
+    }
+}
