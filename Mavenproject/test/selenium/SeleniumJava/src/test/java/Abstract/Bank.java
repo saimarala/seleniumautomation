8c0604bcbc21ed45d12850bd1c1ca045ba0e32bc
saimarala <71916594+saimarala@users.noreply.github.com>
2021-10-19 19:08:40 +0530

    Add files via upload

--- Mavenproject/test/selenium/SeleniumJava/src/test/java/Abstract/Bank.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Abstract/Bank.java
@@ -2,15 +2,16 @@ package Abstract;
 
 public abstract class Bank {
 
-    int amt = 100;
-    final int rate = 10;
-    static int loanrate = 5;
+    int amt = 100;//non static
+    final int rate = 10;//final
+    static int loanrate = 5;//static
 
 
     //partial abstraction
     //hiding the implementation logic ---is called abstraction
     //abstract class can have abstract methods and non abstract methods
     //cannot create the object for abstract class
+    //0-100% abstraction
     public abstract void loan();// abstract method ---no body
 
     //non abstract methods
--- Mavenproject/test/selenium/SeleniumJava/src/test/java/Abstract/HDFCBank.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Abstract/HDFCBank.java
@@ -1,7 +1,8 @@
 package Abstract;
 
 public class HDFCBank extends Bank {
-
+    ////if a class is  extends any abstract class
+    // then its mandatory to define/override all the abstratct methods
     @Override
     public void loan() {
         System.out.println("HDFC--LOAN Method");
@@ -10,7 +11,6 @@ public class HDFCBank extends Bank {
     public void debit() {
         System.out.println("HDFC---debit");
     }
-
     public void roi() {
         System.out.println("HDFC---ROI");
     }
