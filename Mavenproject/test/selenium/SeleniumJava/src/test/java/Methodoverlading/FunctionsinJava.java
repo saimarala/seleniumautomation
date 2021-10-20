9211df4d90e2bdbe3b1908e20835f37384b8337e
saimarala <71916594+saimarala@users.noreply.github.com>
2021-10-20 17:56:25 +0530

    Add files via upload

--- Mavenproject/test/selenium/SeleniumJava/src/test/java/Methodoverlading/FunctionsinJava.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Methodoverlading/FunctionsinJava.java
@@ -1,20 +1,26 @@
 package Methodoverlading;
 
+import java.util.ArrayList;
+
 public class FunctionsinJava {
 
     public static void main(String[] args) {
         //create
         FunctionsinJava obj = new FunctionsinJava();
         obj.test();
-        System.out.println(obj.sum());
+        String s1=obj.getName();
         int a = obj.sum();
         System.out.println("***" + a);
-        String s = obj.PQR();
-        System.out.println(s + " automation");
+        ArrayList<String>ar1=obj.getStudnetNames();
+        System.out.println(ar1);
+        int marks[]=obj.getMarks();
+        for(int ar:marks){
+            System.out.println(ar);
+        }
         int d = obj.div(10, 20);
         System.out.println(d);
-        int d1 = obj.div(30, 50);
-        System.out.println(d1);
+        String country=obj.getCountryName("Pune");
+        System.out.println(country);
 
         //1.cannot create function inside a function
         //2.functions are independent each other
@@ -29,15 +35,52 @@ public class FunctionsinJava {
     public void test() {//0 param
         System.out.println("test method");
     }
-
-    public void test(int i) {//1 param
-        System.out.println("test method");
-
-
+    //2.no input and sum output
+    public String getName() {//1 param
+        System.out.println("get Name");
+        String s="selenium";
+        return s;
+    }
+    public int sum() {
+        System.out.println("sum method");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        return c;
+    }
+    public ArrayList<String> getStudnetNames(){
+        System.out.println("getStudnetNames");
+        ArrayList<String> ar=new ArrayList<String>();
+        ar.add("Sai");
+        ar.add("Selenium");
+        ar.add("Java");
+        return ar;
+    }
+    public int[] getMarks(){
+        System.out.println("get marks method");
+        int marks[]=new int[4];
+        marks[0]=10;
+        marks[1]=7;
+        marks[3]=9;
+        return marks;
+    }
+    //3.some ip and some return
+    public int div(int x, int y) {
+        System.out.println("DIV method");
+        int z = x / y;
+        return z;
     }
 
-    public void test(String i) {//1 param
-        System.out.println("test method");
+    public String getCountryName(String cityName) {
+        System.out.println("getCountryName");
+        if (cityName.equals("bangalore")){
+            return "India";
+        }else if(cityName.equals("london")){
+            return "UK";
+        }else if(cityName.equals("New York")){
+            return "US";
+        }
+        return cityName+" will be added later , currently data is not available";
     }
 
     public void test(int x, int y) {//2 params
@@ -48,34 +91,4 @@ public class FunctionsinJava {
         System.out.println("test method");
     }
 
-    //Method Overloading:when in the same class ,functions/methods are having same name with different parameters
-
-    //2.no input and some output
-    //return type int
-    public int sum() {
-        System.out.println("sum method");
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        return c;
-    }
-
-    public String PQR() {
-        System.out.println("PQR method");
-        String s = "selenium";
-        return s;
-    }
-
-    //3. some input and some output
-    public int div(int x, int y) {
-        System.out.println("DIV method");
-        int z = x / y;
-        return z;
-    }
-
-    public String sendMail(int p, String q) {
-        System.out.println("sendMail method");
-        String h = p + q;
-        return h;
-    }
 }
