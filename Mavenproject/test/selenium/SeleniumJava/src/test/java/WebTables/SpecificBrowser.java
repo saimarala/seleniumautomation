43a540d04fe207d89f606df7c1b3ba69cd36dbaa
saimarala <71916594+saimarala@users.noreply.github.com>
2021-10-22 19:19:52 +0530

    Add files via upload

---
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/WebTables/SpecificBrowser.java
@@ -0,0 +1,35 @@
+package selenium4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WindowType;
+import org.testng.annotations.Test;
+import util.SeleniumBase;
+
+import java.awt.*;
+import java.util.Set;
+
+public class SpecificBrowser extends SeleniumBase {
+
+    @Test
+    void validateTest() throws Throwable {
+        getDriver("https://www.google.com/","chrome");
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("http://demo.automationtesting.in/Register.html");
+        driver.findElement(By.xpath("//*[@placeholder='First Name']")).sendKeys("test");
+        Set<String> winIds=driver.getWindowHandles();
+        for(String winID:winIds){
+            driver.switchTo().window(winID);
+            if(!driver.getTitle().equalsIgnoreCase("OrangeHRM")){
+                driver.close();
+            }
+        }
+
+        Thread.sleep(2000);
+        driver.quit();
+
+
+    }
+}
