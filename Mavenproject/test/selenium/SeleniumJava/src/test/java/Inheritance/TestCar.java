69177251c9e90a0d3d76eac0f1c017d2e19119d5
saimarala <71916594+saimarala@users.noreply.github.com>
2021-11-13 18:21:09 +0530

    Add files via upload

--- Mavenproject/test/selenium/SeleniumJava/src/test/java/Inheritance/TestCar.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Inheritance/TestCar.java
@@ -3,17 +3,21 @@ package Inheritance;
 public class TestCar {
 
     public static void main(String[] args) {
-
+        //We can not overide the static,private and final
         //static polymorphism---compile-time polymorphism
+        //polymorphism means one to many methods(metod overiding)
         BMW obj1 = new BMW();
         obj1.start();
         obj1.stop();
         obj1.theftsafety();
+        obj1.refuel();
+        obj1.engine();
         System.out.println("*******************");
         Car obj2 = new Car();
         obj2.start();
         obj2.stop();
         obj2.refuel();
+        obj2.engine();
 
         System.out.println("****************");
         //child class object referred by  parent class reference variable --dynamic polymorphism--runtime polymorphism
@@ -22,9 +26,17 @@ public class TestCar {
         obj3.start();
         obj3.stop();
         obj3.refuel();
+        obj3.engine();
+
 
         //down casting
         BMW b1 = (BMW) new Car();//classcastException
+        b1.refuel();
+        b1.theftsafety();
+        b1.start();
+        b1.stop();
+        b1.engine();
+
 
     }
 
