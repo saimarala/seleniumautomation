ced9f983e48ad73bd21bc65f1da22cb2254f3f43
saimarala <71916594+saimarala@users.noreply.github.com>
2021-11-29 20:21:48 +0530

    Add files via upload

---
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Testbase/Test1.java
@@ -0,0 +1,15 @@
+package Testbase;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+    void m1(){
+        System.out.println("Sanity test1");
+    }
+    @Test
+    void m2(){
+        System.out.println("Sanity test2");
+    }
+}
---
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Testbase/Test3.java
@@ -0,0 +1,15 @@
+package Testbase;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+    @Test
+    void m1(){
+        System.out.println("Regression test1");
+    }
+    @Test
+    void m2(){
+        System.out.println("Regression test2");
+    }
+}
