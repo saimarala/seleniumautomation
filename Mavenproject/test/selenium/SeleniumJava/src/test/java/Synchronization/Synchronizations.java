2f1bce75b2af9a314d2f939cf4e8d9858bb47925
saimarala <71916594+saimarala@users.noreply.github.com>
2021-12-01 15:30:37 +0530

    Add files via upload

--- Mavenproject/test/selenium/SeleniumJava/src/test/java/Synchronization/Synchronizations.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/Synchronization/Synchronizations.java
@@ -3,6 +3,7 @@ package Synchronization;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -33,11 +34,41 @@ public class Synchronizations {
         driver.findElement(By.xpath("//*[@onclick='swapCheckbox()']")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@onclick='swapCheckbox()']")).click();
+        WebElement ele=driver.findElement(By.xpath("//*[@onclick='swapCheckbox()']"));
 
         //Explicit wait
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         //driver.findElement(By.id("checkbox")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("checkbox"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkbox")));
+        wait.until(ExpectedConditions.alertIsPresent());
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("checkbox")));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(ele));
+
+        wait.until(ExpectedConditions.urlContains("sa"));
+        wait.until(ExpectedConditions.urlToBe("dd"));
+        wait.until(ExpectedConditions.urlMatches("ss"));
+
+        wait.until(ExpectedConditions.titleContains("gg"));
+        wait.until(ExpectedConditions.titleIs("ff"));
+
+        wait.until(ExpectedConditions.attributeContains(By.id("checkbox"),"title","google"));
+        wait.until(ExpectedConditions.attributeToBe(By.id("checkbox"),"id","uName"));
+        wait.until(ExpectedConditions.attributeContains(ele,"title","orange"));
+
+        //wait.until(ExpectedConditions.numberOfElementsToBeLessThan(By.id("checkbox"))).size();
+        wait.until(ExpectedConditions.visibilityOf(ele));
+        wait.until(ExpectedConditions.visibilityOfAllElements(ele));
+
+        wait.until(ExpectedConditions.invisibilityOfAllElements(ele));
+        wait.until(ExpectedConditions.invisibilityOf(ele));
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("checln")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("bb")));
+
+        //wait.until(ExpectedConditions.textMatches(By.id("chech"),"\\s//"));
+        wait.until(ExpectedConditions.textToBePresentInElement(ele,"test"));
+        wait.until(ExpectedConditions.textToBePresentInElementValue(ele,"ff"));
 
         Wait<WebDriver> wait1 = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(10))
