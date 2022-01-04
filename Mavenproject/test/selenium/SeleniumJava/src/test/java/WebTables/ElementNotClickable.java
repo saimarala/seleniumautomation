17abf76843749dac6b5f417ac1116faf52742a18
saimarala <71916594+saimarala@users.noreply.github.com>
2022-01-04 19:38:58 +0530

    Add files via upload

--- Mavenproject/test/selenium/SeleniumJava/src/test/java/WebTables/ElementNotClickable.java
+++ Mavenproject/test/selenium/SeleniumJava/src/test/java/WebTables/ElementNotClickable.java
@@ -17,15 +17,23 @@ public class ElementNotClickable {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://opensource-demo.orangehrmlive.com/");
-        List<WebElement> btn=driver.findElements(By.xpath("//input[@id='btnLogin']"));
-        for(int i=0;i<btn.size();i++){
-            WebElement e=btn.get(i);
-            int x=btn.get(i).getLocation().getX();
-            if(x!=0){{
-                btn.get(i).click();
+        driver.get("http://seleniumpractise.blogspot.com/2016/08/how-to-automate-radio-button-in.html");
+        List<WebElement> btn=driver.findElements(By.id("male"));
+//        for(int i=0;i<btn.size();i++){
+//            WebElement e=btn.get(i);
+//            int x=btn.get(i).getLocation().getX();
+//            if(x!=0){{
+//                btn.get(i).click();
+//                break;
+//            }
+//            }
+//        }
+
+        for (WebElement ele:btn){
+            int x=ele.getLocation().getX();
+            if(x!=0){
+                ele.click();
                 break;
-            }
             }
         }
          Thread.sleep(3000);
